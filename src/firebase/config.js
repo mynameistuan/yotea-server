942846b00858303924e49106f5034c91d385c41b
Lê Văn Tuân <51940082+letuan02@users.noreply.github.com>
2022-08-11 21:38:36 +0700

    update

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,6 @@
-const admin = require("firebase-admin");
+import admin from "firebase-admin";
 
-const serviceAccount = require("./serviceAccountKey.json");
+import serviceAccount from "./serviceAccountKey.json";
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
