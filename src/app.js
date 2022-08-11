8922981b75baa7978f6610cecbc7dc3922c845ef
Lê Văn Tuân <51940082+letuan02@users.noreply.github.com>
2022-08-11 22:50:14 +0700

    update

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://trasuayotea.vercel.app",
   },
 });
 
